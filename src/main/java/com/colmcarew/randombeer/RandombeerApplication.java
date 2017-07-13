a19b4c74929c2e25300a4e90f3386682c94ba211
Colm Carew <colmcarew2@gmail.com>
2017-07-13 01:31:03 +0100

    Initial Spring Boot Project Creation - SpringBoot v 2 M2

---
+++ src/main/java/com/colmcarew/randombeer/RandombeerApplication.java
@@ -0,0 +1,12 @@
+package com.colmcarew.randombeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandombeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandombeerApplication.class, args);
+	}
+}
